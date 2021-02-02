1ed2148a994564c1573531cbd836dda06072a3ec
wenjia <micheal_wen@outlook.com>
2021-02-02 15:39:30 +0800

    [add]初始代码，蓝牙连接测试成功，交互测试成功

---
+++ src/Program.cs
@@ -0,0 +1,272 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System;
+//using System.Collections.Generic;
+//using System.Configuration;
+//using InTheHand.Net;
+//using InTheHand.Net.Bluetooth;
+//using InTheHand.Net.Sockets;
+//using System.IO;
+
+//namespace ConsoleApp1
+//{
+//    class LanYa
+//    {
+//        public string blueName { get; set; } //蓝牙名字
+//        public BluetoothAddress blueAddress { get; set; } //蓝牙的唯一标识符
+//        public ClassOfDevice blueClassOfDevice { get; set; } //蓝牙是何种类型
+//        public bool IsBlueAuth { get; set; } //指定设备通过验证
+//        public bool IsBlueRemembered { get; set; } //记住设备
+//        public DateTime blueLastSeen { get; set; }
+//        public DateTime blueLastUsed { get; set; }
+//    }
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            BluetoothRadio radio = BluetoothRadio.PrimaryRadio;//获取蓝牙适配器
+//            if (radio == null)
+//            {
+//                Console.WriteLine("没有找到本机蓝牙设备!");
+//            }
+//            else
+//            {
+//                Console.WriteLine("ClassOfDevice: " + radio.ClassOfDevice);
+//                Console.WriteLine("HardwareStatus: " + radio.HardwareStatus);
+//                Console.WriteLine("HciRevision: " + radio.HciRevision);
+//                Console.WriteLine("HciVersion: " + radio.HciVersion);
+//                Console.WriteLine("LmpSubversion: " + radio.LmpSubversion);
+//                Console.WriteLine("LmpVersion: " + radio.LmpVersion);
+//                Console.WriteLine("LocalAddress: " + radio.LocalAddress);
+//                Console.WriteLine("Manufacturer: " + radio.Manufacturer);
+//                Console.WriteLine("Mode: " + radio.Mode);
+//                Console.WriteLine("Name: " + radio.Name);
+//                Console.WriteLine("Remote:" + radio.Remote);
+//                Console.WriteLine("SoftwareManufacturer: " + radio.SoftwareManufacturer);
+//                Console.WriteLine("StackFactory: " + radio.StackFactory);
+//            }
+//            //Console.ReadKey();
+
+
+//            List<LanYa> lanYaList = new List<LanYa>(); //搜索到的蓝牙的集合
+//            BluetoothClient client = new BluetoothClient();
+//            radio.Mode = RadioMode.Connectable;
+//            BluetoothDeviceInfo[] devices = client.DiscoverDevices();//搜索蓝牙 10秒钟
+//            int count = 0;
+//            foreach (var item in devices)
+//            {
+//                count++;
+//                Console.WriteLine("===========蓝牙设备" + count + "================");
+//                Console.WriteLine("device name:" + item.DeviceName);//输出每个蓝牙设备的名字
+//                Console.WriteLine("device address:" + item.DeviceAddress);//输出每个蓝牙设备的名字
+//                Console.WriteLine("ClassOfDevice:" + item.ClassOfDevice);
+//                Console.WriteLine("Authenticated:" + item.Authenticated);
+//                Console.WriteLine("Remembered:" + item.Remembered);
+//                Console.WriteLine("LastSeen:" + item.LastSeen);
+//                Console.WriteLine("LastUsed:" + item.LastUsed);
+//                lanYaList.Add(new LanYa { blueName = item.DeviceName, blueAddress = item.DeviceAddress, blueClassOfDevice = item.ClassOfDevice, IsBlueAuth = item.Authenticated, IsBlueRemembered = item.Remembered, blueLastSeen = item.LastSeen, blueLastUsed = item.LastUsed });//把搜索到的蓝牙添加到集合中
+//            }
+//            Console.WriteLine("device count:" + devices.Length);//输出搜索到的蓝牙设备个数
+
+
+//            ////蓝牙的配对
+//            //BluetoothClient blueclient = new BluetoothClient();
+//            //Guid mGUID1 = BluetoothService.Handsfree; //蓝牙服务的uuid
+
+//            //blueclient.Connect(s.blueAddress, mGUID) //开始配对 蓝牙4.0不需要setpin
+
+
+//            BluetoothDeviceInfo dev = devices[0];
+
+//            //客户端
+//            BluetoothClient bl = new BluetoothClient();//
+//            Guid mGUID = Guid.Parse("0000fff4-0000-1000-8000-00805F9B34FB");//蓝牙串口服务的uuiid
+
+//            //byte[] def = { 0x13, 0x00, 0x05, 0x15, 0x11, 0x08, 0x01, 0x10, 0x02, 0x01, 0x12, 0x14 };
+
+//            //string s = "D05FB81A21CE";
+//            //byte[] def = Encoding.Default.GetBytes(s);
+//            //string str2 = BitConverter.ToString(def);
+//            //Console.WriteLine(str2);
+
+//            //BluetoothAddress abc = new BluetoothAddress(def);
+
+//            //try
+//            //{
+//            //    //bl.Connect(abc, mGUID);
+//            //    bl.Connect(dev.DeviceAddress, mGUID);
+//            //    Console.WriteLine("连接成功");
+//            //    //"连接成功";
+//            //}
+//            //catch (Exception x)
+//            //{
+//            //    Console.WriteLine("连接异常");
+//            //    //异常
+//            //}
+
+//            //var v = bl.GetStream();
+//            //byte[] sendData = Encoding.Default.GetBytes(“人生苦短，我用python”);
+//            //v.Write(sendData, 0, sendData.Length); //发送
+
+
+//            //服务器端
+//            //BluetoothListener bluetoothListener = new BluetoothListener(mGUID);
+//            //bluetoothListener.Start();//开始监听
+
+//            //bl = bluetoothListener.AcceptBluetoothClient();//接收
+
+
+//            //while (true)
+//            //{
+//            //    Console.WriteLine("111111111");
+//            //    byte[] buffer = new byte[100];
+//            //    Stream peerStream = bl.GetStream();
+
+//            //    peerStream.Read(buffer, 0, buffer.Length);
+
+//            //    string data = Encoding.UTF8.GetString(buffer).ToString().Replace("\0", "");//去掉后面的\0字节
+//            //}
+//            Console.ReadKey();
+
+//        }
+//    }
+//}
+
+
+
+
+//using System;
+//using System.IO;
+//using System.Net.Sockets;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading;
+//// [注意1]：要添加如下三个命名空间 
+//using InTheHand.Net;
+//using InTheHand.Net.Bluetooth;
+//using InTheHand.Net.Sockets;
+//namespace TestBluetooth
+//{
+//    class Program
+//    {
+//        static void Main(string[] args) { 
+//            BluetoothRadio bluetoothRadio = BluetoothRadio.PrimaryRadio; 
+//            if (bluetoothRadio == null) { 
+//                Console.WriteLine("没有找到本机蓝牙设备!"); 
+//            } else { 
+//                Program p = new Program(); 
+//                p.localAdapterInfo(bluetoothRadio); 
+//                p.openDoor(); 
+//            } 
+//        } /** * 连接目标蓝牙设备发送开门指令 * **/
+//        private void openDoor()
+//        {
+//            BluetoothClient cli = new BluetoothClient(); BluetoothAddress addr = null; BluetoothEndPoint ep = null; try
+//            { // [注意2]：要注意MAC地址中字节的对应关系，直接来看顺序是相反的，例如 // 如下对应的MAC地址为——12:34:56:78:9a:bc 
+//                addr = new BluetoothAddress(new byte[] { 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12 }); ep = new BluetoothEndPoint(addr, BluetoothService.SerialPort); cli.Connect(ep); // 连接蓝牙 
+//                if (cli.Connected)
+//                {
+//                    Stream peerStream = cli.GetStream(); peerStream.WriteByte(0xBB); // 发送开门指令 
+//                }
+//            }
+//            catch (SocketException e) { Console.WriteLine(e.Message); }
+//            finally
+//            {
+//                if (cli != null)
+//                { // [注意3]：要延迟一定时间(例如1000毫秒) //避免因连接后又迅速断开而导致蓝牙进入异常(**)状态 
+//                    Thread.Sleep(1000); cli.Close();
+//                }
+//            }
+//        } /** * * 显示本地蓝牙的信息 * * **/
+//        private void localAdapterInfo(BluetoothRadio bluetoothRadio) { 
+//            Console.WriteLine("ClassOfDevice: " + bluetoothRadio.ClassOfDevice); 
+//            Console.WriteLine("HardwareStatus: " + bluetoothRadio.HardwareStatus); 
+//            Console.WriteLine("HciRevision: " + bluetoothRadio.HciRevision); 
+//            Console.WriteLine("HciVersion: " + bluetoothRadio.HciVersion); 
+//            Console.WriteLine("LmpSubversion: " + bluetoothRadio.LmpSubversion); 
+//            Console.WriteLine("LmpVersion: " + bluetoothRadio.LmpVersion); 
+//            Console.WriteLine("LocalAddress: " + bluetoothRadio.LocalAddress); 
+//            Console.WriteLine("Manufacturer: " + bluetoothRadio.Manufacturer); 
+//            Console.WriteLine("Mode: " + bluetoothRadio.Mode); 
+//            Console.WriteLine("Name: " + bluetoothRadio.Name); 
+//            Console.WriteLine("Remote:" + bluetoothRadio.Remote); 
+//            Console.WriteLine("SoftwareManufacturer: " + bluetoothRadio.SoftwareManufacturer); 
+//            Console.WriteLine("StackFactory: " + bluetoothRadio.StackFactory); 
+//        }
+//    }
+//}
+
+using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tmp = CommunicateHexProtocolDecoder.Code(0x00, 0x00, 0x0002, new byte[] { });
+            //BLECode.BluetoothLECode ble = new BLECode.BluetoothLECode("00001800-0000-1000-8000-00805f9b34fb", "00002a00-0000-1000-8000-00805f9b34fb", "00002a01-0000-1000-8000-00805f9b34fb");
+            BLECode.BluetoothLECode ble = new BLECode.BluetoothLECode("0000fff0-0000-1000-8000-00805f9b34fb", "0000fff1-0000-1000-8000-00805f9b34fb", "0000fff1-0000-1000-8000-00805f9b34fb");
+            ble.ValueChanged += Ble_ValueChanged;
+            //ble.SelectDeviceFromIdAsync("d3:89:67:45:23:ab");
+            ble.StartBleDeviceWatcher();
+            while (true) Thread.Sleep(1000);
+        }
+
+        private static void Ble_ValueChanged(BLECode.MsgType type, string content, byte[] data = null)
+        {
+            if(content.Contains("Radar"))
+            {
+                Console.WriteLine("=================================");
+                Console.WriteLine(content);
+                string id = content.Substring(content.LastIndexOf("####")+4);
+                Console.WriteLine("Connecting " + id);
+                Console.WriteLine("=================================");
+                System.Threading.Tasks.Task.Factory.StartNew(() => 
+                {
+                    BLECode.BluetoothLECode radarBLE = new BLECode.BluetoothLECode("0000fff0-0000-1000-8000-00805f9b34fb", "0000fff1-0000-1000-8000-00805f9b34fb", "0000fff1-0000-1000-8000-00805f9b34fb");
+                    radarBLE.ValueChanged += (MsgType, msg, replayData) => 
+                        {
+                            if (replayData != null)
+                            {
+                                Console.WriteLine(msg + "应答：" + BitConverter.ToString(replayData));
+                                if (replayData[replayData.Length-1] == 0xAA && replayData[replayData.Length-2] == 0xAA)
+                                    radarBLE.Dispose();
+                            }
+                            else
+                            {
+                                Console.WriteLine(msg);
+                            }
+                        };
+                    radarBLE.SelectDeviceFromIdAsync(id);
+                    while (radarBLE.CurrentWriteCharacteristic == null) Thread.Sleep(10);
+                    Console.WriteLine("-------------------------------获取版本号-------------------------------");
+                    radarBLE.Write(CommunicateHexProtocolDecoder.Code(0x00, 0x00, 0x0002, new byte[] { }));
+                    //Console.ReadKey();
+                    //Console.WriteLine("==============重启=================");
+                    //radarBLE.Write(CommunicateHexProtocolDecoder.Code(0x00, 0x00, 0x0005, new byte[] { }));
+                    //Console.ReadKey();
+                    //Console.WriteLine("==============重启=================");
+                    //radarBLE.Write(CommunicateHexProtocolDecoder.Code(0x00, 0x00, 0x0005, new byte[] { }));
+                    //Console.ReadKey();
+                    //Console.WriteLine("==============重启=================");
+                    //radarBLE.Write(CommunicateHexProtocolDecoder.Code(0x00, 0x00, 0x0005, new byte[] { }));
+                    //Console.ReadKey();
+                    //Console.WriteLine("==============重启=================");
+                    //radarBLE.Write(CommunicateHexProtocolDecoder.Code(0x00, 0x00, 0x0005, new byte[] { }));
+                    //Console.ReadKey();
+                });
+            }
+        }
+    }
+}
---
+++ src/ProtocolDecoder.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class CommunicateHexProtocolDecoder
+    {
+        private const byte HEADER = 0x55;
+        private const byte FOOT = 0xAA;
+        public const byte SUCCESS = 0x00;
+
+        private const byte MinLength = 0x0B;
+
+        public const byte ADDRESS_PUBLIC = 0xFF;
+
+        public const byte ERROVERTIME = 0x05;
+
+        private const ushort FUNCTION_HARD_VER = 0x0001;
+        private const ushort FUNCTION_SOFT_VER = 0x0002;
+
+        public const ushort FUNCTION_UPDATE_TEST = 0x00F0;
+        public const ushort FUNCTION_UPDATE_ERASE = 0x00F1;
+        public const ushort FUNCTION_UPDATE_SIZE = 0x00F2;
+        public const ushort FUNCTION_UPDATE_POSITION_DATA = 0x00F3;
+        public const ushort FUNCTION_UPDATE_CRC = 0x00F4;
+        public const ushort FUNCTION_UPDATE_DATA = 0x00F5;
+        public const ushort FUNCTION_UPDATE_READ_FLASH = 0x00F6;
+        public const ushort FUNCTION_UPDATE_REFRESH = 0x00F8;
+        public const ushort FUNCTION_UPDATE_RESTART = 0x00F9;
+
+        static byte[] Crc8_table = {
+                0x93,0x98,0xE4,0x46,0xEB,0xBA,0x04,0x4C,
+                0xFA,0x40,0xB8,0x96,0x0E,0xB2,0xB7,0xC0,
+                0x0C,0x32,0x9B,0x80,0xFF,0x30,0x7F,0x9D,
+                0xB3,0x81,0x58,0xE7,0xF1,0x19,0x7E,0xB6,
+                0xCD,0xF7,0xB4,0xCB,0xBC,0x5C,0xD6,0x09,
+                0x20,0x0A,0xE0,0x37,0x51,0x67,0x24,0x95,
+                0xE1,0x62,0xF8,0x5E,0x38,0x15,0x54,0x77,
+                0x63,0x57,0x6D,0xE9,0x89,0x76,0xBE,0x41,
+                0x5D,0xF9,0xB1,0x4D,0x6C,0x53,0x9C,0xA2,
+                0x23,0xC4,0x8E,0xC8,0x05,0x42,0x61,0x71,
+                0xC5,0x00,0x18,0x6F,0x5F,0xFB,0x7B,0x11,
+                0x65,0x2D,0x8C,0xED,0x14,0xAB,0x88,0xD5,
+                0xD9,0xC2,0x36,0x34,0x7C,0x5B,0x3C,0xF6,
+                0x48,0x0B,0xEE,0x02,0x83,0x79,0x17,0xE6,
+                0xA8,0x78,0xF5,0xD3,0x4E,0x50,0x52,0x91,
+                0xD8,0xC6,0x22,0xEC,0x3B,0xE5,0x3F,0x86,
+                0x06,0xCF,0x2B,0x2F,0x3D,0x59,0x1C,0x87,
+                0xEF,0x4F,0x10,0xD2,0x7D,0xDA,0x72,0xA0,
+                0x9F,0xDE,0x6B,0x75,0x56,0xBD,0xC7,0xC1,
+                0x70,0x1D,0x25,0x92,0xA5,0x31,0xE2,0xD7,
+                0xD0,0x9A,0xAF,0xA9,0xC9,0x97,0x08,0x33,
+                0x5A,0x99,0xC3,0x16,0x84,0x82,0x8A,0xF3,
+                0x4A,0xCE,0xDB,0x29,0x0F,0xAE,0x6E,0xE3,
+                0x8B,0x07,0x3A,0x74,0x47,0xB0,0xBB,0xB5,
+                0x7A,0xA9,0x2C,0xD4,0x03,0x3E,0x1A,0xA7,
+                0x27,0x64,0x06,0xBF,0x56,0x73,0x1E,0xFE,
+                0x49,0x01,0x39,0x28,0xF4,0x26,0xDF,0xDD,
+                0x44,0x0D,0x21,0xF2,0x85,0xB9,0xEA,0x4B,
+                0xDC,0x6A,0xCA,0xAC,0x12,0xFC,0x2E,0x2A,
+                0xA3,0xF0,0x66,0xE8,0x60,0x45,0xA1,0x8D,
+                0x68,0x35,0xFD,0x8F,0x9E,0x1F,0x13,0xD1,
+                0xAD,0x69,0xCC,0xA4,0x94,0x90,0x1B,0x43,
+        };
+        static byte Crc8(byte[] data)
+        {
+            byte crc = 0;
+            foreach (byte val in data)
+            {
+                crc = Crc8_table[crc ^ val];
+            }
+            return crc;
+        }
+        internal static byte[] Code(byte address, byte error, ushort function, byte[] data)
+        {
+            byte[] message = new byte[2 + 1 + 1 + 2 + 2 + data.Length + 1 + 2];
+            message[0] = HEADER;
+            message[1] = HEADER;
+            message[2] = address;
+            message[3] = error;
+
+            byte[] MsgData = new byte[1 + 1 + 2 + 2 + data.Length];
+            MsgData[0] = address;
+            MsgData[1] = error;
+            Array.Copy(BitConverter.GetBytes(function), 0, MsgData, 2, 2);
+            Array.Copy(BitConverter.GetBytes((ushort)data.Length), 0, MsgData, 4, 2);
+            Array.Copy(data, 0, MsgData, 6, data.Length);
+
+            Array.Copy(MsgData, 0, message, 2, MsgData.Length);
+            message[message.Length - 1] = 0xAA;
+            message[message.Length - 2] = 0xAA;
+            message[message.Length - 3] = Crc8(MsgData);
+            return message;
+        }
+
+        public Action NotifyCRCError { get; set; }
+        public Action<byte> NotifyReplayError { get; set; }
+
+        public Action<Tuple<byte/*address*/, byte/*error*/, ushort/*function*/, byte[]/*data*/>> NotifyDecodeResult { get; set; }
+        public Action<byte[]> NotifyFullResult { get; set; }
+        internal void Decode(byte[] frame)
+        {
+            if (frame[0] == HEADER && frame[1] == HEADER && frame[frame.Length - 1] == FOOT && frame[frame.Length - 2] == FOOT)
+            {
+                byte[] data = new byte[frame.Length - 2 - 2 - 1];
+                Array.Copy(frame, 2, data, 0, data.Length);
+                if (CheckCRC(data, frame[frame.Length - 3]))
+                {
+                    if (data[1] != SUCCESS)
+                    {
+                        NotifyReplayError(data[1]);
+                    }
+                    else
+                    {
+                        byte[] values = new byte[data.Length - 6];
+                        Array.Copy(data, 6, values, 0, values.Length);
+                        NotifyDecodeResult?.Invoke(new Tuple<byte, byte, ushort, byte[]>(data[0], data[1], BitConverter.ToUInt16(data, 2), data));
+                    }
+                }
+                else
+                {
+                    NotifyCRCError?.Invoke();
+                }
+            }
+            else
+            {
+                throw new Exception("Frame Decode Error");
+            }
+        }
+
+        private bool CheckCRC(byte[] data, byte val)
+        {
+            return (val == 0xFF) ? true : (Crc8(data) == val);
+        }
+
+        private int decodeCount = 0;
+        public int MaxCantDecodeCount = 100;
+        internal void Decode(ref ConcurrentQueue<byte> data)
+        {
+            lock (data)
+            {
+                byte headerCount = 0, index = 0, tmp = 0;
+                int frameStart = 0;
+                if (data.Count >= GenerateLength(0))
+                {
+                    foreach (byte val in data)
+                    {
+                        index++;
+                        if (val == HEADER)
+                        {
+                            if (headerCount == 0)
+                                frameStart = index;
+                            headerCount++;
+                        }
+                        if (headerCount == 0)
+                        {
+                            frameStart++;
+                        }
+                        else if (headerCount == 1)
+                        {
+                            if (index > (frameStart + 1))
+                            {
+                                headerCount = 0;
+                            }
+                        }
+                        else if (headerCount == 2)
+                        {
+                            break;
+                        }
+                    }
+                    //discard no use data in front
+                    for (int i = 0; i < frameStart - 1; i++)
+                    {
+                        data.TryDequeue(out tmp);
+                    }
+                    index = 0;
+                    byte lengthLow = 0x00, lengthHigh = 0x00;
+                    int framelength = 0;
+                    bool errorFrame = false;
+                    int frameEnd = -1;
+                    foreach (byte val in data)
+                    {
+                        index++;
+                        if (index == 7)
+                            lengthLow = val;
+                        else if (index == 8)
+                        {
+                            lengthHigh = val;
+                        }
+                        else if (index == 9)
+                        {
+                            framelength = GenerateLength((lengthHigh << 8) + lengthLow);
+                            frameEnd = 2 + 1 + 1 + 2 + 2 + (lengthHigh << 8) + lengthLow + 1 + 1;
+                        }
+                        else if (index == frameEnd)
+                        {
+                            errorFrame = (val != FOOT);
+                        }
+                        else if (index == (frameEnd + 1))
+                        {
+                            errorFrame = errorFrame && (val != FOOT);
+                            break;
+                        }
+                    }
+                    if (index == (frameEnd + 1))
+                    {
+                        if (errorFrame)
+                        {
+                            data.TryDequeue(out tmp);
+                        }
+                        else
+                        {
+                            byte[] frame = new byte[framelength];
+                            for (int i = 0; i < framelength; i++)
+                            {
+                                data.TryDequeue(out frame[i]);
+                            }
+                            if (framelength == 0)
+                            {
+                                int stophere = 0;
+                            }
+                            System.Threading.Tasks.Task.Factory.StartNew(() => { Decode(frame); });
+                            //Decode(frame);
+                            decodeCount = 0;
+                        }
+                    }
+                }
+
+                decodeCount++;
+                if (decodeCount > MaxCantDecodeCount)
+                {
+                    decodeCount = 0;
+                    for (int i = 0; i < data.Count; i++)
+                    {
+                        data.TryDequeue(out tmp);
+                    }
+                }
+
+            }
+        }
+
+        private int GenerateLength(int dataLen)
+        {
+            return 2/*header*/+ 1/*address*/+ 1/*error*/+ 2/*function*/+ 2/*length*/+ dataLen + 1/*crc*/+ 2/*foot*/;
+        }
+    }
+}
